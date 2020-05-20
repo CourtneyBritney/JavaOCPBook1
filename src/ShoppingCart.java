212b46989ea9eb59227857555ba767203276b1ee
CourtneyBritney <courtney02alexander@gmail.com>
2020-05-20 14:50:20 +0200

    Exercise 4

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,5 +1,22 @@
 public class ShoppingCart {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Shopping Cart!");
+        String custName = "Courtney Alexander";
+        String itemDesc = "Sunflower dress";
+        String message = custName+" wants to purchase a "+itemDesc;
+
+        double price = 39.99;
+        int quantity = 2;
+        double tax = 2.04;
+        double total;
+
+        message = custName+" wants to purchase "+quantity+ " "+itemDesc;
+
+        System.out.println(message);
+
+        total = quantity * price * tax;
+
+        System.out.println("Total cost with tax: "+total);
     }
+
 }
